9fbbf0a5d7373cf43684cf04075785199919fe8c
S-Mohsen-H <seyyedmohsenhosainy@gmail.com>
2025-02-12 09:49:34 +0330

    1403/11/24

--- lib/tasks/Tasks.h
+++ lib/tasks/Tasks.h
@@ -55,7 +55,6 @@ extern void readJoystick_task(void *arg);
 extern void bluetoothManager_task(void *arg);
 extern void ledManager_task(void *arg);
 extern void taskManager_task(void *arg);
-extern void ledManager_task(void *arg);
 
 /**
  * @brief Checks if any elements of the byte array is 0d26 (0x1A)
